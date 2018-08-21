9471a599e36e5996958a3febc45107067b4f4c5b
Hana Navratilova <HanaN@kentico.com>
2018-08-21 10:55:25 +0200

    KA-324 add ListController

---
+++ MyPerfectOnboardingApplication/MyPerfectOnboarding.Api.Tests/Controllers/ListControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Build.Tasks;
+using MyPerfectOnboarding.Api.Controllers;
+using NUnit.Framework;
+
+namespace MyPerfectOnboarding.Api.Tests.Controllers
+{
+    [TestFixture]
+    class ListControllerTests
+    {
+        [Test]
+        public async Task Get_ReturnsListOfItems()
+        {
+            var listController = new ListController();
+
+            listController.Request = new HttpRequestMessage();
+            listController.Configuration = new HttpConfiguration();
+
+            var response = await listController.GetAsync();
+
+            var message = await response.ExecuteAsync(CancellationToken.None);
+
+            message.TryGetContentValue(out string[] items);
+
+            Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+         
+            Assert.That(items, Is.EqualTo(new[]{ "aaaa", "lll" }));
+        }
+    }
+}
---
+++ MyPerfectOnboardingApplication/MyPerfectOnboarding.Api/Controllers/ListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MyPerfectOnboarding.Api.Controllers
+{
+    public class ListController : ApiController
+    {
+
+        public async Task<IHttpActionResult> GetAsync()
+        {
+            var items = new[] {"aaaa", "lll"};
+            return await Task.FromResult((IHttpActionResult)Ok(items));
+        }
+    }
+}
