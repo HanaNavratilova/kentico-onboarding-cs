29ffd5683e64a19b2aa6cc1672e3f009b6fec9fe
Hana Navratilova <HanaN@kentico.com>
2018-09-17 17:36:45 +0200

    KA-325 use NSubstitute in tests

--- MyPerfectOnboardingApplication/MyPerfectOnboarding.Api.Tests/Controllers/ListControllerTests.cs
+++ MyPerfectOnboardingApplication/MyPerfectOnboarding.Api.Tests/Controllers/ListControllerTests.cs
@@ -19,8 +19,8 @@ namespace MyPerfectOnboarding.Api.Tests.Controllers
         private ListController _listController;
 
         private readonly ListItem[] _items = {
-            new ListItem{Id = Guid.NewGuid(), Text = "aaaaa", IsActive = false, CreationTime = new DateTime(1589, 12, 3), LastUpdateTime = new DateTime(1896, 4, 7)},
-            new ListItem{Id = Guid.NewGuid(), Text = "dfads", IsActive = false, CreationTime = new DateTime(4568, 6, 23), LastUpdateTime = new DateTime(8569, 8, 24)},
+            new ListItem{Id = new Guid("0B9E6EAF-83DC-4A99-9D57-A39FAF258CAC"), Text = "aaaaa", IsActive = false, CreationTime = new DateTime(1589, 12, 3), LastUpdateTime = new DateTime(1896, 4, 7)},
+            new ListItem{Id = new Guid("11AC59B7-9517-4EDD-9DDD-EB418A7C1644"), Text = "dfads", IsActive = false, CreationTime = new DateTime(4568, 6, 23), LastUpdateTime = new DateTime(8569, 8, 24)},
         };
 
         private IListRepository _repository;
@@ -40,21 +40,27 @@ namespace MyPerfectOnboarding.Api.Tests.Controllers
         [Test]
         public async Task Get_ReturnsListOfItems()
         {
+            _repository.GetAllItemsAsync().Returns(Task.FromResult(_items));
+
             var message = await _listController.ExecuteAction(controller => controller.GetAsync());
 
-           message.TryGetContentValue(out List<ListItem> items);
+           message.TryGetContentValue(out IEnumerable<ListItem> items);
 
             Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(items, Is.EqualTo(_items));
         }
 
         [Test]
         public async Task Get_ReturnsItemWithGivenId()
         {
-            var id = Guid.NewGuid();
+            var expectedItem = _items[0];
+            _repository.GetItemAsync(expectedItem.Id).Returns(Task.FromResult(expectedItem));
 
-            var message = await _listController.ExecuteAction(controller => controller.GetAsync(id));
+            var message = await _listController.ExecuteAction(controller => controller.GetAsync(expectedItem.Id));
+            message.TryGetContentValue(out ListItem item);
 
             Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(item, Is.EqualTo(expectedItem));
         }
 
         [Test]
@@ -71,21 +77,22 @@ namespace MyPerfectOnboarding.Api.Tests.Controllers
         public async Task Put_OkReturned()
         {
             var id = Guid.NewGuid();
-
-            var item = new ListItem{ Id = id, Text = "newItem", IsActive = false, CreationTime = DateTime.Now, LastUpdateTime = DateTime.Now };
+            var item = new ListItem{Text = "newItem"};
 
             var message = await _listController.ExecuteAction(controller => controller.PutAsync(id, item));
 
+            await _repository.Received().EditItemAsync(id, item);
             Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
         }
 
         [Test]
         public async Task Delete_NoContentReturned()
         {
-            var id = Guid.NewGuid();
+            Guid id = new Guid("22AC59B7-9517-4EDD-9DDD-EB418A7C1678");       
 
             var message = await _listController.ExecuteAction(controller => controller.DeleteAsync(id));
 
+            await _repository.Received().DeleteItemAsync(id);
             Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
         }
     }
