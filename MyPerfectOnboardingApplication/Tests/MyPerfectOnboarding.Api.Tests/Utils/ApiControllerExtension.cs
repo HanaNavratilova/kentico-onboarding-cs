15b03c23eb17cf713f52682c57bc46ea9e7f6139
Hana Navratilova <HanaN@kentico.com>
2018-10-04 12:20:07 +0200

    KA-326 code refactoring - name lambda function in ServicesBootstraper - rename WebApiConfig.cs -> RoutesConfig.cs - rename types -> registeredTypes in ContainerConfigTests - add EqualConstraintExtension.cs (UsingComparer function) - rename ListControllerExtension.cs -> ApiControllerExtension.cs (making ExecuteAction more generics) - code formating

--- MyPerfectOnboardingApplication/Tests/MyPerfectOnboarding.Api.Tests/Utils/ApiControllerExtension.cs
+++ MyPerfectOnboardingApplication/Tests/MyPerfectOnboarding.Api.Tests/Utils/ApiControllerExtension.cs
@@ -3,13 +3,12 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
-using MyPerfectOnboarding.Api.Controllers;
 
 namespace MyPerfectOnboarding.Api.Tests.Utils
 {
-    public static class ListControllerExtension
+    public static class ApiControllerExtension
     {
-        public static async Task<HttpResponseMessage> ExecuteAction(this ListController controller, Func<ListController, Task<IHttpActionResult>> action)
+        public static async Task<HttpResponseMessage> ExecuteAction(this ApiController controller, Func<ApiController, Task<IHttpActionResult>> action)
         {
             var response = await action(controller);
 
---
+++ MyPerfectOnboardingApplication/Tests/MyPerfectOnboarding.Api.Tests/Utils/EqualConstraintExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework.Constraints;
+
+namespace MyPerfectOnboarding.Api.Tests.Utils
+{
+    public static class EqualConstraintExtension
+    {
+        private static readonly Lazy<ListItemEqualityComparer> LazyComparer = new Lazy<ListItemEqualityComparer>();
+
+        public static EqualConstraint UsingComparer(this EqualConstraint equalConstraint)
+            => equalConstraint.Using(LazyComparer.Value);
+    }
+}
